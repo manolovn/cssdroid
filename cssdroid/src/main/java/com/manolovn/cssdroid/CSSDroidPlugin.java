6efa0d44486a5e48fb465af78294e07fe1895333
Manolo Vera <manolo.vn@gmail.com>
2016-01-15 00:44:23 +0100

    added configuration extension

--- cssdroid/src/main/java/com/manolovn/cssdroid/CSSDroidPlugin.java
+++ cssdroid/src/main/java/com/manolovn/cssdroid/CSSDroidPlugin.java
@@ -1,5 +1,6 @@
 package com.manolovn.cssdroid;
 
+import com.manolovn.cssdroid.config.CSSDroidExtension;
 import com.manolovn.cssdroid.task.ParseCSSTask;
 
 import org.gradle.api.Plugin;
@@ -10,8 +11,12 @@ import org.gradle.api.Project;
  */
 public class CSSDroidPlugin implements Plugin<Project> {
 
+    private static final String EXTENSION = "cssdroid";
+    private static final String TASK = "CSSDROIDGENERATE";
+
     @Override
     public void apply(Project project) {
-        project.getTasks().create("cssdroidGenerate", ParseCSSTask.class);
+        project.getExtensions().create(EXTENSION, CSSDroidExtension.class);
+        project.getTasks().create(TASK, ParseCSSTask.class);
     }
 }
---
+++ cssdroid/src/main/java/com/manolovn/cssdroid/config/CSSDroidExtension.java
@@ -0,0 +1,26 @@
+package com.manolovn.cssdroid.config;
+
+/**
+ * CSSDroid configuration extension
+ */
+public class CSSDroidExtension {
+
+    private String cssDir = "/../src/main/acss/";
+    private String outputDir = "/../src/main/res/values/";
+
+    public String getCssDir() {
+        return cssDir;
+    }
+
+    public void setCssDir(String cssDir) {
+        this.cssDir = cssDir;
+    }
+
+    public String getOutputDir() {
+        return outputDir;
+    }
+
+    public void setOutputDir(String outputDir) {
+        this.outputDir = outputDir;
+    }
+}
