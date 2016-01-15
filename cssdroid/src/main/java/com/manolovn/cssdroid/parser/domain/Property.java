7cdd395758589dd6ecfdcf171709dd67d3d8064c
Manolo Vera <manolo.vn@gmail.com>
2016-01-15 01:04:42 +0100

    added basic entities

---
+++ cssdroid/src/main/java/com/manolovn/cssdroid/parser/domain/Property.java
@@ -0,0 +1,23 @@
+package com.manolovn.cssdroid.parser.domain;
+
+/**
+ * Property entity
+ */
+public class Property {
+
+    private final String name;
+    private final String value;
+
+    public Property(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ cssdroid/src/main/java/com/manolovn/cssdroid/parser/domain/Selector.java
@@ -0,0 +1,17 @@
+package com.manolovn.cssdroid.parser.domain;
+
+/**
+ * Selector entity
+ */
+public class Selector {
+
+    private final String name;
+
+    public Selector(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
