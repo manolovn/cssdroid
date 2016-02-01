fdfcc769b3327ce4de24317890d1687c8f8045e7
Manolo Vera <manolo.vn@gmail.com>
2016-02-01 01:19:42 +0100

    simple android style writer

---
+++ cssdroid/src/main/java/com/manolovn/cssdroid/translator/AndroidStyleWriter.java
@@ -0,0 +1,33 @@
+package com.manolovn.cssdroid.translator;
+
+import com.manolovn.cssdroid.util.StringUtils;
+
+/**
+ * Android styles writer
+ */
+public class AndroidStyleWriter {
+
+    public String start() {
+        return "<resources>\n";
+    }
+
+    public String end(String input) {
+        return input + "</resources>\n";
+    }
+
+    public String newLine() {
+        return "\n";
+    }
+
+    public String openStyle(String style) {
+        return "    <style name=\"" + StringUtils.capitalize(style) + "\">\n";
+    }
+
+    public String closeStyle() {
+        return "    </style>\n";
+    }
+
+    public String addProperty(String name, String value) {
+        return "        <item name=\"android:" + name + "\">" + value + "</item>\n";
+    }
+}
