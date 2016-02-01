7bc11441d0ec451e62a7e0a740ed5481056c08c3
Manolo Vera <manolo.vn@gmail.com>
2016-02-01 01:20:41 +0100

    first attempt of style generation

---
+++ cssdroid/src/main/java/com/manolovn/cssdroid/translator/StylesheetToXMLTranslator.java
@@ -0,0 +1,30 @@
+package com.manolovn.cssdroid.translator;
+
+import com.manolovn.cssdroid.parser.domain.Node;
+import com.manolovn.cssdroid.parser.domain.StyleSheet;
+
+/**
+ * CSS Stylesheet to XML translator
+ */
+public class StylesheetToXMLTranslator {
+
+    private final AndroidStyleWriter writer;
+
+    public StylesheetToXMLTranslator() {
+        writer = new AndroidStyleWriter();
+    }
+
+    public String translate(StyleSheet styleSheet) {
+        String output = writer.start();
+        output += writer.newLine();
+        for (Node node : styleSheet.getRules()) {
+            output += writer.openStyle(node.getName().replace(".", ""));
+            for(Node child : node.children()) {
+                output += writer.addProperty(child.getName().replace("-", "_"), child.getValue());
+            }
+            output += writer.closeStyle();
+            output += writer.newLine();
+        }
+        return writer.end(output);
+    }
+}
